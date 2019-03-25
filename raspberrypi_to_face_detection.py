087589935f618f9ad2433661efd8183c1f01e449
Harsh <aravsharma798@gmail.com>
2019-03-25 22:15:43 +0530

    Add files via upload

---
+++ raspberrypi_to_face_detection.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+
+face_cascade = cv2.CascadeClassifier('face.xml')
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    img = frame.array
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    #rawCapture.truncate(0)
+
+    if key == ord("q"):
+    		break
+
